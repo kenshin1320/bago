8ea0d84bbff9d69e2140964294af1a1420f39320
Kenneth <yalongkennethyalong@gmail.com>
2024-06-07 00:26:26 +0800

    mapping next

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 20, 109, 26)),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 62, 14, 109)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'FlutDemo Home Page'),
@@ -25,7 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -35,45 +33,66 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 5;
 
+  var _data = [
+    {
+      "img": 'https://picsum.photos/250?image=9',
+      "pamagat": "laputhgvgjbhop",
+    },
+    {
+      "img": 'https://picsum.photos/250?image=9',
+      "pamagat": "lasdadedrftgyhujikolp",
+    }
+  ];
+
   void _incrementCounter() {
     setState(() {
-     
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
- 
     return Scaffold(
       appBar: AppBar(
-       
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
         title: Text(widget.title),
       ),
-      body: Center(
-       
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-              
+      //   body: Center(
 
-            )
-          ],
-        ),
-      ),
+      //     child: Column(
+
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: <Widget>[
+      //         const Text(
+      //           'You have pushed the button this many times:',
+      //         ),
+      //         Text(
+      //           '$_counter',
+      //           style: Theme.of(context).textTheme.headlineMedium,
+      //         ),
+      //         Text(
+      //           '$_counter',
+      //           style: Theme.of(context).textTheme.headlineMedium,
+
+      //         )
+      //       ],
+      //     ),
+
+      //   ),
+      body: Center(
+          child: Column(
+        children: <Widget>[
+          Container(
+            child: Image.network(_data[0]["img"] as String),
+          ),
+          Container(
+              child: Text(
+            _data[0]["pamagat"].toString(),
+            textAlign: TextAlign.left,
+          ))
+        ],
+      )),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
